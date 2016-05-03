67c66aa199acf2df06b58876d1e7acc9f3a8336b
profzoidberg <ryanlynn523@gmail.com>
2016-05-03 12:10:08 -0400

    append list

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 	for (var i = 1; i < 101; i++) {
-			console.log(i)
-		if ((i % 3 === 0 && i % 5 === 0)) {
-		console.log ('fizzbuzz');
-		}
-		else if ( i % 3 === 0) { 
-			console.log('fizz');
-		}
-		else if ((i % 5 === 0)) {
-			console.log('buzz');
+		console.log(i)
+		$('.fizzbuzz').append('<li> '+i+' </li>'); {
+			if ((i % 3 === 0 && i % 5 === 0)) {
+				console.log('fizzbuzz');
+			}
+			else if ( i % 3 === 0) { 
+				console.log('fizz');
+			}
+			else if ((i % 5 === 0)) {
+				console.log('buzz');
+			}
 		}
 	}
 });
