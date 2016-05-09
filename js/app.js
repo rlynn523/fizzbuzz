6f2a721c91e51c0b8e9e7dc942cbe9f4cbf9a6a4
profzoidberg <ryanlynn523@gmail.com>
2016-05-09 18:08:33 -0400

    cancel button

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
 $(document).ready(function(){
-	var input = parseInt(prompt("Please enter a number in the box below"));
-	if (isNaN(input)) {
-		alert("Not a number!");
-	}
-	for (var i = 1; i <= input; i++) {
-		if ((i % 3 === 0 && i % 5 === 0)) {
-			$('.fizzbuzz').append('<li> fizzbuzz </li>');
-		} else if ( i % 3 === 0) { 
-			$('.fizzbuzz').append('<li> fizz </li>');
-		} else if (i % 5 === 0) {
-			$('.fizzbuzz').append('<li> buzz </li>');
-		} else {
-			$('.fizzbuzz').append('<li> '+i+' </li>');
+	function getInput(){
+		var input = prompt("Please enter a number in the box below");
+		if (input === null) {
+			console.log("cancel");
+			return;
+		} else if (isNaN(input)) {
+			alert("Not a number!");
+			getInput();
+			console.log("notANumber");
+		} else { 
+			fizzBuzz(input);
+			console.log("fizzBuzzInput");
 		}
 	}
+	function fizzBuzz(input){ 
+		for (var i = 1; i <= input; i++) {
+			if ((i % 3 === 0 && i % 5 === 0)) {
+				$('.fizzbuzz').append('<li> fizzbuzz </li>');
+			} else if ( i % 3 === 0) { 
+				$('.fizzbuzz').append('<li> fizz </li>');
+			} else if (i % 5 === 0) {
+				$('.fizzbuzz').append('<li> buzz </li>');
+			} else {
+				$('.fizzbuzz').append('<li> '+i+' </li>');
+			}
+		}
+	}
+	getInput();
 });
