a4366dd25edafcc72780025fe0d28cc406398411
profzoidberg <ryanlynn523@gmail.com>
2016-05-10 22:12:38 -0400

    refactoring/arrays

--- js/app.js
+++ js/app.js
@@ -2,27 +2,28 @@ $(document).ready(function(){
 	function getInput(){
 		var input = prompt("Please enter a number in the box below");
 		if (input === null) {
-			console.log("cancel");
 			return;
 		} else if (isNaN(input)) {
 			alert("Not a number!");
 			getInput();
-			console.log("notANumber");
 		} else { 
 			fizzBuzz(input);
-			console.log("fizzBuzzInput");
 		}
+	}
+	function appBuzz(output) {
+		$('.fizzbuzz').append(a);
 	}
 	function fizzBuzz(input){ 
 		for (var i = 1; i <= input; i++) {
+			var output = ['<li> fizzbuzz </li>','<li> fizz </li>', '<li> buzz </li>','<li> '+i+' </li>']
 			if ((i % 3 === 0 && i % 5 === 0)) {
-				$('.fizzbuzz').append('<li> fizzbuzz </li>');
-			} else if ( i % 3 === 0) { 
-				$('.fizzbuzz').append('<li> fizz </li>');
+				appBuzz(a=output[0]);
+			} else if (i % 3 === 0) { 
+				appBuzz(a=output[1]);
 			} else if (i % 5 === 0) {
-				$('.fizzbuzz').append('<li> buzz </li>');
+				appBuzz(a=output[2]);
 			} else {
-				$('.fizzbuzz').append('<li> '+i+' </li>');
+				appBuzz(a=output[3]);
 			}
 		}
 	}
