4e37fcc2f05c6fec17ef13224c57ed2d769af42e
profzoidberg <ryanlynn523@gmail.com>
2016-05-12 10:10:22 -0400

    parseInt

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	function getInput(){
-		var input = prompt("Please enter a number in the box below");
-		if (input === null) {
+		var input = parseInt(prompt("Please enter a number in the box below") || 0);
+		if (input === 0) {
+			console.log('input');
 			return;
 		} else if (isNaN(input)) {
 			alert("Not a number!");
@@ -10,20 +11,21 @@ $(document).ready(function(){
 			fizzBuzz(input);
 		}
 	}
-	function appBuzz(output) {
-		$('.fizzbuzz').append(a);
-	}
 	function fizzBuzz(input){ 
 		for (var i = 1; i <= input; i++) {
 			var output = ['<li> fizzbuzz </li>','<li> fizz </li>', '<li> buzz </li>','<li> '+i+' </li>']
+			function appBuzz(output) {
+				$('.fizzbuzz').append(output);
+				console.log(output);
+			}
 			if ((i % 3 === 0 && i % 5 === 0)) {
-				appBuzz(a=output[0]);
+				appBuzz(output[0]);
 			} else if (i % 3 === 0) { 
-				appBuzz(a=output[1]);
+				appBuzz(output[1]);
 			} else if (i % 5 === 0) {
-				appBuzz(a=output[2]);
+				appBuzz(output[2]);
 			} else {
-				appBuzz(a=output[3]);
+				appBuzz(output[3]);
 			}
 		}
 	}
